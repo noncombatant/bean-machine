1425b66aba6dece359fec84b40dc1b4328b08766
Chris Palmer <chris@noncombatant.org>
2017-10-08 16:43:32 -0700

    Add `getTerms` and a test.
    Parses tokens returned from `getTokens` into terms.
    
    Some cleanup in the test code, too.

--- new-search.js
+++ new-search.js
@@ -1,3 +1,5 @@
+"use strict";
+
 // query := terms
 // terms := term
 //     | terms
@@ -32,7 +34,7 @@ const getTokens = function(string) {
   let currentToken = ""
   for (let i = 0; i < string.length; ++i) {
     const c = string[i]
-    const next = i < (string.length - 1) ? string[i + 1] : ''
+    const next = i < (string.length - 1) ? string[i + 1] : ""
 
     // Allow escaped quotes.
     if ('\\' === c && '"' === next) {
@@ -69,4 +71,43 @@ const getTokens = function(string) {
   }
   pushIf(tokens, currentToken)
   return tokens
+}
+
+const getTerms = function(tokens) {
+  const terms = []
+  for (let i = 0; i < tokens.length; ++i) {
+    let token = tokens[i]
+    if (0 === token.length) {
+      throw "Invalid token. Bug in `getTokens`."
+    }
+    const next = i < (tokens.length - 1) ? tokens[i + 1] : ""
+    const term = { property: "", value: "", negated: false }
+
+    if ("-" === token[0]) {
+      if (1 === token.length) {
+        continue
+      }
+      token = token.substring(1)
+      term.negated = true
+    }
+
+    if (token.endsWith(":")) {
+      if (1 === token.length) {
+        continue
+      }
+      token = token.substring(0, token.length - 1)
+      term.property = token
+      token = next
+      ++i
+    }
+
+    // Allow a 0-length value if there is a property, to enable tests for
+    // property existence. Otherwise, require a non-empty token.
+    if (0 === term.property.length && 0 === token.length) {
+      throw "Invalid token. Bug in `getTokens`."
+    }
+    term.value = token
+    terms.push(term)
+  }
+  return terms
 }
