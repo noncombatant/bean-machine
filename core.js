86d6b0e2dbe512e3383c7664d7e150009a68eff2
Chris Palmer <chris@noncombatant.org>
2017-11-09 20:26:20 -0800

    Move `displayNowPlaying` to core.js.

--- core.js
+++ core.js
@@ -10,3 +10,21 @@ const resetSearchHits = function() {
   }
   return hits
 }
+
+const displayNowPlaying = function(item, element) {
+  removeAllChildren(element)
+
+  const trackName = item[Name] || basename(item[Pathname])
+  element.appendChild(createElement("span", "", item[Disc] + "-" + item[Track] + " “" + trackName + "”\u200A—\u200A"))
+  element.appendChild(createElement("strong", "", item[Artist]))
+  element.appendChild(createElement("span", "", "\u200A—\u200A"))
+  element.appendChild(createElement("em", "", item[Album]))
+
+  document.title = element.textContent
+
+  // TODO: Re-enable this when fully supported. As of December 2016, Firefox
+  // ignores `silent`, and Chrome for Android throws an "illegal constructor"
+  // exception.
+  //const icon = dirname(player.src) + "/cover.jpg"
+  //showNotification(document.title, { silent: true, icon: icon, badge: icon })
+}
