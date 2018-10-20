f608c80dead9c7871a50a60bb6b013e170a3e4a4
Chris Palmer <chris@noncombatant.org>
2018-10-20 10:57:44 -0700

    Search the Year field too.
    Not sure why I turned that off.

--- search.js
+++ search.js
@@ -58,7 +58,7 @@ const parseTerms = function(string) {
 
 const itemMatches = function(terms, item) {
   const delimiter = "\x00"
-  const all = normalizeStringForSearch(item[Pathname] + delimiter + item[Artist] + delimiter + item[Album] + delimiter + item[Name] + delimiter + item[Genre])
+  const all = normalizeStringForSearch(item[Pathname] + delimiter + item[Artist] + delimiter + item[Album] + delimiter + item[Name] + delimiter + item[Genre] + delimiter + item[Year])
   for (let i = 0; i < terms.length; ++i) {
     let t = terms[i]
     const negated = "-" === t[0]
