cfd98a86ad7c6b24d51122fc277af41e10138588
Chris Palmer <chris@noncombatant.org>
2020-04-18 10:05:30 -0700

    Don't cache anything in the service worker.
    Not sure yet why it makes the installed PWA so flaky on Android.

--- sw.js
+++ sw.js
@@ -25,23 +25,23 @@ const PRECACHE_URLS = [
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(PRECACHE)
-      .then(cache => cache.addAll(PRECACHE_URLS))
+//      .then(cache => cache.addAll(PRECACHE_URLS))
       .then(self.skipWaiting())
   )
 })
 
 // The activate handler takes care of cleaning up old caches.
 self.addEventListener('activate', event => {
-  const currentCaches = [PRECACHE, RUNTIME]
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return cacheNames.filter(cacheName => !currentCaches.includes(cacheName))
-    }).then(cachesToDelete => {
-      return Promise.all(cachesToDelete.map(cacheToDelete => {
-        return caches.delete(cacheToDelete)
-      }))
-    }).then(() => self.clients.claim())
-  )
+//  const currentCaches = [PRECACHE, RUNTIME]
+//  event.waitUntil(
+//    caches.keys().then(cacheNames => {
+//      return cacheNames.filter(cacheName => !currentCaches.includes(cacheName))
+//    }).then(cachesToDelete => {
+//      return Promise.all(cachesToDelete.map(cacheToDelete => {
+//        return caches.delete(cacheToDelete)
+//      }))
+//    }).then(() => self.clients.claim())
+//  )
 })
 
 // The fetch handler serves responses for same-origin resources from a cache.
@@ -52,20 +52,26 @@ self.addEventListener('fetch', event => {
   if (!event.request.url.startsWith(self.location.origin)) {
     return
   }
-  event.respondWith(
-    caches.match(event.request).then(cachedResponse => {
-      if (cachedResponse) {
-        return cachedResponse
-      }
 
-      return caches.open(RUNTIME).then(cache => {
-        return fetch(event.request).then(response => {
-          // Put a copy of the response in the runtime cache.
-          return cache.put(event.request, response.clone()).then(() => {
-            return response
-          })
-        })
-      })
-    })
-  )
+  // Just do a stupid pass-through, no caching.
+  return fetch(event.request).then(response => {
+    return response
+  })
+
+//  event.respondWith(
+//    caches.match(event.request).then(cachedResponse => {
+//      if (cachedResponse) {
+//        return cachedResponse
+//      }
+//
+//      return caches.open(RUNTIME).then(cache => {
+//        return fetch(event.request).then(response => {
+//          // Put a copy of the response in the runtime cache.
+//          return cache.put(event.request, response.clone()).then(() => {
+//            return response
+//          })
+//        })
+//      })
+//    })
+//  )
 })
