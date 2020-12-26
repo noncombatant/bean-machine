b7bfff157002db46cd5f4c58cd9b12c672ec0c3e
Chris Palmer <chris@noncombatant.org>
2020-12-26 14:09:01 -0800

    Service Worker was getting in the way of my caching.
    Caching attempts, that is. Simplify the `fetch` handler to do basically nothing.
    
    Some clean-up.

--- web/sw.js
+++ web/sw.js
@@ -1,3 +1,6 @@
+// Originally adapted from a Service Worker example from Google, which carried
+// this license:
+//
 // Copyright 2016 Google Inc. All Rights Reserved. Licensed under the Apache
 // License, Version 2.0 (the "License"); you may not use this file except in
 // compliance with the License. You may obtain a copy of the License at
@@ -7,32 +10,38 @@
 // KIND, either express or implied. See the License for the specific language
 // governing permissions and limitations under the License.
 
-// Names of the two caches used in this version of the service worker. Change to
-// v2, etc. when you update any of the local resources, which will in turn
-// trigger the install event again.
-const PRECACHE = 'precache-v1'
-const RUNTIME = 'runtime'
+"use strict";
 
-// A list of local resources we always want to be cached.
-const PRECACHE_URLS = [
-  'index.html',
-  './', // Alias for index.html
-  'index.css',
-  'index.js',
+const PreCache = "precache-v2"
+const RunTimeCache = "runtime"
+
+const PreCacheURLs = [
+  "./", // Alias for index.html
+  "clef-512.png",
+  "close.png",
+  "help.png",
+  "index.css",
+  "index.html",
+  "index.js",
+  "pause.png",
+  "play.png",
+  "repeat.png",
+  "search.png",
+  "skip.png",
+  "shuffle.png",
 ]
 
-// The install handler takes care of precaching the resources we always need.
-self.addEventListener('install', event => {
+self.addEventListener("install", event => {
+console.log("install")
   event.waitUntil(
-    caches.open(PRECACHE)
-//      .then(cache => cache.addAll(PRECACHE_URLS))
+    caches.open(PreCache)
+      .then(cache => cache.addAll(PreCacheURLs))
       .then(self.skipWaiting())
   )
 })
 
-// The activate handler takes care of cleaning up old caches.
-self.addEventListener('activate', event => {
-//  const currentCaches = [PRECACHE, RUNTIME]
+self.addEventListener("activate", event => {
+//  const currentCaches = [PreCache, RunTimeCache]
 //  event.waitUntil(
 //    caches.keys().then(cacheNames => {
 //      return cacheNames.filter(cacheName => !currentCaches.includes(cacheName))
@@ -44,27 +53,23 @@ self.addEventListener('activate', event => {
 //  )
 })
 
-// The fetch handler serves responses for same-origin resources from a cache.
-// If no response is found, it populates the runtime cache with the response
-// from the network before returning it to the page.
-self.addEventListener('fetch', event => {
-  // Skip cross-origin requests, like those for Google Analytics.
-  if (!event.request.url.startsWith(self.location.origin)) {
-    return
-  }
-
-  // Just do a stupid pass-through, no caching.
-  return fetch(event.request, {"credentials": "include"}).then(response => {
-    return response
-  })
-
+self.addEventListener("fetch", event => {
+//  if (!event.request.url.startsWith(self.location.origin)) {
+//    return
+//  }
+//
+//  // Just do a stupid pass-through, no caching.
+//  return fetch(event.request, {"credentials": "include"}).then(response => {
+//    return response
+//  })
+//
 //  event.respondWith(
 //    caches.match(event.request).then(cachedResponse => {
 //      if (cachedResponse) {
 //        return cachedResponse
 //      }
 //
-//      return caches.open(RUNTIME).then(cache => {
+//      return caches.open(RunTimeCache).then(cache => {
 //        return fetch(event.request).then(response => {
 //          // Put a copy of the response in the runtime cache.
 //          return cache.put(event.request, response.clone()).then(() => {
