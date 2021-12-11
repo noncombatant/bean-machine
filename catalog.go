aba018ea11946f617990c61f2949c37c8aa6220e
Chris Palmer <chris@noncombatant.org>
2021-12-11 13:31:05 -0800

    Simplify reading and writing the catalog.
    Also clean up some logging.

--- catalog.go
+++ catalog.go
@@ -38,23 +38,46 @@ const (
 	catalogFileTemp = "catalog.gobs.tmp"
 )
 
-func (c *Catalog) buildCatalogFromGobs(gobs *os.File, modified time.Time) {
+func (c *Catalog) readCatalog(gobs *os.File) {
 	decoder := gob.NewDecoder(gobs)
-	newInfos := ItemInfos{}
-	for {
-		var info ItemInfo
-		e := decoder.Decode(&info)
-		if e != nil {
-			if e == io.EOF {
-				break
-			} else {
-				Logger.Fatal("decode error:", e)
-			}
-		}
-		newInfos = append(newInfos, &info)
+	infos := ItemInfos{}
+	e := decoder.Decode(&infos)
+	if e != nil && e != io.EOF {
+		Logger.Fatal(e)
 	}
-	c.ItemInfos = newInfos
-	c.Modified = modified
+	c.ItemInfos = infos
+}
+
+func (c *Catalog) writeCatalog(root string, infos ItemInfos) {
+	catalogFileTempPath := path.Join(root, catalogFileTemp)
+	catalogFilePath := path.Join(root, catalogFile)
+	gobs, e := os.OpenFile(catalogFileTempPath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+	if e != nil {
+		Logger.Fatal(e)
+	}
+
+	status, e := gobs.Stat()
+	if e != nil {
+		Logger.Fatal(e)
+	} else {
+		c.Modified = status.ModTime()
+	}
+
+	encoder := gob.NewEncoder(gobs)
+	e = encoder.Encode(infos)
+	if e != nil {
+		Logger.Fatal(e)
+	}
+
+	e = gobs.Close()
+	if e != nil {
+		Logger.Fatal(e)
+	}
+
+	e = os.Rename(catalogFileTempPath, catalogFilePath)
+	if e != nil {
+		Logger.Fatal(e)
+	}
 }
 
 func shouldSkipFile(pathname string, info os.FileInfo) bool {
@@ -71,23 +94,6 @@ func (c *Catalog) buildCatalogFromWalk(root string) {
 		c.buildCatalogFromWalkInProgress = false
 	}()
 
-	catalogFileTempPath := path.Join(root, catalogFileTemp)
-	catalogFilePath := path.Join(root, catalogFile)
-	gobs, e := os.OpenFile(catalogFileTempPath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
-	if e != nil {
-		Logger.Fatal(e)
-	}
-	// We do `gobs.Close()` explicitly at the end.
-
-	status, e := gobs.Stat()
-	if e != nil {
-		Logger.Printf("Can't Stat %q: %v", catalogFileTemp, e)
-	} else {
-		c.Modified = status.ModTime()
-	}
-
-	encoder := gob.NewEncoder(gobs)
-
 	// Log the walk progress periodically so that the operator knows what’s going
 	// on.
 	count := 0
@@ -95,7 +101,7 @@ func (c *Catalog) buildCatalogFromWalk(root string) {
 	timer := time.NewTimer(timerFrequency)
 
 	newItems := ItemInfos{}
-	e = filepath.Walk(root,
+	e := filepath.Walk(root,
 		func(pathname string, info os.FileInfo, e error) error {
 			if e != nil {
 				Logger.Printf("%q: %s", pathname, e)
@@ -120,10 +126,6 @@ func (c *Catalog) buildCatalogFromWalk(root string) {
 				itemInfo.ModTime = fmt.Sprintf("%04d-%02d-%02d", time.Year(), time.Month(), time.Day())
 				itemInfo.fillMetadata()
 				newItems = append(newItems, &itemInfo)
-				e := encoder.Encode(itemInfo)
-				if e != nil {
-					Logger.Fatal(e)
-				}
 
 				count++
 				select {
@@ -143,16 +145,11 @@ func (c *Catalog) buildCatalogFromWalk(root string) {
 		})
 
 	if e != nil {
-		Logger.Printf("Problem walking %q: %s", root, e)
+		Logger.Print(e)
 	}
 	c.buildCatalogFromWalkInProgress = false
-	gobs.Close()
-	e = os.Rename(catalogFileTempPath, catalogFilePath)
-	if e != nil {
-		Logger.Fatal(e)
-	}
+	c.writeCatalog(root, newItems)
 	c.ItemInfos = newItems
-	Logger.Printf("Completed. %v items.", len(c.ItemInfos))
 }
 
 func (c *Catalog) BuildCatalog(root string) {
@@ -176,7 +173,8 @@ func (c *Catalog) BuildCatalog(root string) {
 
 	modified := status.ModTime()
 	if c.Modified.IsZero() || c.Modified.Before(modified) {
-		c.buildCatalogFromGobs(gobs, modified)
+		c.readCatalog(gobs)
+		c.Modified = modified
 		return
 	}
 }
