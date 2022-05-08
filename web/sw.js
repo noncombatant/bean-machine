3d6e4be56e31ed11fac92698d81917ee314995bd
Chris Palmer <chris@noncombatant.org>
2022-05-08 13:07:40 -0700

    Fix Service Worker
    Needed to create and invoke an `async` function as the argument to
    `event.respondWith`.

--- web/sw.js
+++ web/sw.js
@@ -1,18 +1,43 @@
+// Copyright 2022 by Chris Palmer (https://noncombatant.org), and released under
+// the terms of the GNU GPL3. See web/index.html for more information.
+
 "use strict";
 
-self.addEventListener("install", event => {})
+const cacheName = "cache"
 
-self.addEventListener("activate", event => {})
+self.addEventListener("install", event => {
+  //console.log("install")
+  // If we wanted to pre-cache anything:
+  //event.waitUntil((async () => {
+  //  const cache = await caches.open(cacheName);
+  //  await cache.addAll(contentToCache);
+  //})());
+})
+
+self.addEventListener("register", event => {
+  //console.log("register")
+})
+
+self.addEventListener("activate", event => {
+  //console.log("activate")
+})
 
 self.addEventListener("fetch", event => {
-  event.respondWith(
-    const c = await caches.open("cache")
-    let r = await c.match(event.request)
+  //console.log("fetch")
+  event.respondWith((async () => {
+    //console.log("responding...")
+    let r = await caches.match(event.request)
+    //console.log("cached response?", r)
     if (r) {
+      //console.log("returning cached response")
       return r
     }
+
     r = await fetch(event.request)
+    //console.log("fetched response", r)
+    const c = await caches.open(cacheName)
     c.put(event.request, r.clone())
+    //console.log("put cloned response, returning r")
     return r
-  )
+  })())
 })
